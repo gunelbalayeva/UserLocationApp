b4a26bd2c5000239af0b79c29fac3a052b913990
gunelbalayeva <gunelbalayeva97@gmail.com>
2025-11-28 20:32:33 +0400

    Initial Commit

---
+++ UserLocationApp/UserLocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationAppApp.swift
+//  UserLocationApp
+//
+//  Created by User on 28.11.25.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
